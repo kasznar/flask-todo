aec3a113360928534888b96babcbae7830f72c87
Kasznár Dávid <kasznardavid8@gmail.com>
2023-03-18 23:12:41 +0100

    centralize database connection logic

--- todo.py
+++ todo.py
@@ -1,7 +1,7 @@
 import os
 import sqlite3
 
-from flask import Flask, render_template, request, abort, redirect
+from flask import Flask, render_template, request, abort, redirect, g
 
 # create application object
 app = Flask(__name__, instance_relative_config=True)
@@ -10,20 +10,35 @@ app = Flask(__name__, instance_relative_config=True)
 DATABASE = os.path.join(app.instance_path, 'todo.sqlite')
 
 
+def get_db():
+    if 'db' not in g:
+        g.db = sqlite3.connect(
+            DATABASE,
+            detect_types=sqlite3.PARSE_DECLTYPES
+        )
+
+    return g.db
+
+
+def close_db(e=None):
+    db = g.pop('db', None)
+
+    if db is not None:
+        db.close()
+
+
+# this will run at the end of each request
+app.teardown_appcontext(close_db)
+
+
 @app.route("/")
 def index():
-    db = sqlite3.connect(
-        DATABASE,
-        detect_types=sqlite3.PARSE_DECLTYPES
-    )
-
+    db = get_db()
     todos = db.execute(
         'SELECT id, created, description'
         ' FROM todo'
         ' ORDER BY created DESC'
     ).fetchall()
-
-    db.close()
 
     return render_template('index.html', todos=todos)
 
@@ -35,21 +50,13 @@ def create():
     if not description:
         abort(400)
 
-    db = sqlite3.connect(
-        DATABASE,
-        detect_types=sqlite3.PARSE_DECLTYPES
-    )
-
-    print(description)
-
+    db = get_db()
     db.execute(
         'INSERT INTO todo (description)'
         'VALUES (?)',
         (description,)
     )
     db.commit()
-
-    db.close()
 
     return redirect('/')
 
@@ -61,29 +68,21 @@ def update(id):
     if not description:
         abort(400)
 
-    db = sqlite3.connect(
-        DATABASE,
-        detect_types=sqlite3.PARSE_DECLTYPES
-    )
+    db = get_db()
     db.execute(
         'UPDATE todo SET description = ?'
         ' WHERE id = ?',
         (description, id)
     )
     db.commit()
-    db.close()
 
     return redirect('/')
 
 
 @app.post("/delete/<int:id>")
 def delete(id):
-    db = sqlite3.connect(
-        DATABASE,
-        detect_types=sqlite3.PARSE_DECLTYPES
-    )
+    db = get_db()
     db.execute('DELETE FROM todo WHERE id = ?', (id,))
     db.commit()
-    db.close()
 
     return redirect('/')
